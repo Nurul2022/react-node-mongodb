56d5b121ac3a9bf21529d017140401c4a0f55097
Nurul Islam <nurulbdpersonal@gmail.com>
2022-04-26 12:16:18 +0600

    first commit

---
+++ src/components/AddUser/AddUser.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+
+const AddUser = () => {
+
+    const handleAddUser = (event) => {
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+
+        const user = { name, email };
+
+        // send data to the server
+        fetch('http://localhost:5000/user', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('success', data);
+                alert('users added successfully!!!');
+                event.target.reset();
+            })
+
+    }
+    return (
+        <div>
+            <h3>Please add a new user</h3>
+            <form onSubmit={handleAddUser}>
+                <input type="text" name="name" placeholder='Name' required />
+                <br />
+                <input type="email" name="email" placeholder='Email' required />
+                <br />
+                <input type="submit" value="Add User" />
+            </form>
+        </div>
+    );
+};
+
+export default AddUser;
